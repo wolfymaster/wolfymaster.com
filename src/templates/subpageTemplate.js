0ef90f7b412eba7ec163743efbc2c4b6c82a669b
Paul Sherer <wolfymaster@hotmail.com>
2019-11-29 21:25:11 -0500

    add branding colors to scss add menuBackground to frontmatter for changing the menu opacity background

--- src/templates/subpageTemplate.js
+++ src/templates/subpageTemplate.js
@@ -7,19 +7,21 @@ import Footer from '../components/footer';
 
 const subpageTemplate = ({children, pageContext:{frontmatter}}) => {
 
+    let menuBackground = frontmatter.menuBackground || "rgba(167,76,67,0.5)";
+
     console.log(frontmatter, frontmatter.hasBanner === true);
 
     return (
         <>
             <Container fluid={true}>
-                <Row style={{background: "#a74c43"}}>
+                <Row style={frontmatter.hasBanner === true ? {background: menuBackground } : {background: "#a74c43"}}>
                     <Col lg={{span: 4, offset: 8}}>
                         <Menu/>
                     </Col>
                 </Row>
             </Container>
 
-            <Container fluid={frontmatter.hasBanner === true}>
+            <Container fluid={frontmatter.hasBanner === true} style={frontmatter.hasBanner === true ? {marginTop:"-56px", zIndex:-1, position:"relative"} : {}}>
                 <Row>
                     <div style={ frontmatter.hasBanner === true ? {width:"100%"} : {margin: "50px auto"}}>
                         {children}
