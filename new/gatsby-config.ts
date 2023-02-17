5447c37a91055f0eb226a25776b4aec66724fb42
wolfy <wolfy@wolfymaster.com>
2023-02-17 08:33:04 -0600

    Add Flexiblog theme + updates to make it run with Gatsby v5.x.x

--- new/gatsby-config.ts
+++ new/gatsby-config.ts
@@ -1,29 +1,116 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
+  graphqlTypegen: true,
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-google-gtag",
+    "gatsby-plugin-image",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+      __key: "pages"
+    },
+    {
+      resolve: '@elegantstack/gatsby-theme-flexiblog-education',
+      options: {
+        // Add theme options here. Check documentation for available options.
+        siteUrl: process.env.URL || process.env.VERCEL_URL
+      }
+    }],
   siteMetadata: {
     title: `wolfymaster.com`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
-  graphqlTypegen: true,
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-mdx", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
-    },
-    __key: "pages"
-  }]
+    siteUrl: `https://www.wolfymaster.com`,
+    name: 'WolfyMaster',
+    description: 'My site description...',
+    email: '',
+    address: '',
+    phone: '',
+
+    //Site Social Media Links
+    social: [
+      {
+        name: 'Github',
+        url: 'https://github.com/gatsbyjs'
+      },
+      {
+        name: 'Twitter',
+        url: 'https://twitter.com/gatsbyjs'
+      },
+      {
+        name: 'Instagram',
+        url: 'https://github.com/gatsbyjs'
+      }
+    ],
+
+    //Header Menu Items
+    headerMenu: [
+      {
+        name: 'Home',
+        slug: '/'
+      },
+      {
+        name: 'Tutors',
+        slug: '/authors'
+      },
+      {
+        name: 'Contact',
+        slug: '/contact'
+      }
+    ],
+
+    //Footer Menu Items (2 Sets)
+    footerMenu: [
+      {
+        title: 'Quick Links',
+        items: [
+          {
+            name: 'Advertise with us',
+            slug: '/contact'
+          },
+          {
+            name: 'About Us',
+            slug: '/about'
+          },
+          {
+            name: 'Contact Us',
+            slug: '/contact'
+          }
+        ]
+      },
+      {
+        title: 'Legal Stuff',
+        items: [
+          {
+            name: 'Privacy Notice',
+            slug: '/'
+          },
+          {
+            name: 'Cookie Policy',
+            slug: '/'
+          },
+          {
+            name: 'Terms Of Use',
+            slug: '/'
+          }
+        ]
+      }
+    ]
+  }
 };
 
 export default config;
